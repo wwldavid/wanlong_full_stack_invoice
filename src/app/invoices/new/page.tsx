7f819c2cc8611115d4ec699ee49615c8a134ccc4
davidwanlongwu <davidwu.web@gmail.com>
2025-04-28 22:10:41 -0600

    basic invoice form structure

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,47 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col h-full justify-center max-w-5xl mx-auto gap-8 my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Create Invoice</h1>
+      </div>
+
+      <form className="grid gap-4 max-w-xs">
+        <div>
+          <Label htmlFor="name" className="block font-semibold text-sm mb-2">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="email" className="block font-semibold text-sm mb-2">
+            Billing Email
+          </Label>
+          <Input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <Label htmlFor="value" className="block font-semibold text-sm mb-2">
+            Value
+          </Label>
+          <Input id="value" name="value" type="text" />
+        </div>
+        <div>
+          <Label
+            htmlFor="description"
+            className="block font-semibold text-sm mb-2"
+          >
+            Description
+          </Label>
+          <Textarea id="description" name="description"></Textarea>
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="max-w-5xl mx-auto text-center bg-lime-100">
+    <main className="flex flex-col h-screen justify-center max-w-5xl mx-auto text-center gap-8 bg-teal-100">
       <h1 className="text-5xl font-bold">Invoice Pad</h1>
       <p>
-        <a href="/dashboard">Sign In</a>
+        <Button asChild>
+          <Link href="/dashboard">Sign In</Link>
+        </Button>
       </p>
     </main>
   );
