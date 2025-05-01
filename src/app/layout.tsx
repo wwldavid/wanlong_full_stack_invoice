bb16eca3b4c9e31c928123172d6c23c07094b1b7
davidwanlongwu <davidwu.web@gmail.com>
2025-05-01 10:53:15 -0600

    add clerk login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="antialiased">{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex flex-col h-screen justify-center max-w-5xl mx-auto text-center gap-8 bg-teal-100">
       <h1 className="text-5xl font-bold">Invoice Pad</h1>
       <p>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
