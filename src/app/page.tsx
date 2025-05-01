da9c5416d806454de6ce1812214d282396219635
davidwanlongwu <davidwu.web@gmail.com>
2025-05-01 11:18:22 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import {
 export default function Home() {
   return (
     <main className="flex flex-col h-screen justify-center max-w-5xl mx-auto text-center gap-8 bg-teal-100">
-      <h1 className="text-5xl font-bold">Invoice Pad</h1>
+      <h1 className="text-5xl font-bold">Invoice Notebook</h1>
       <p>
         <SignedOut>
           <SignInButton />
