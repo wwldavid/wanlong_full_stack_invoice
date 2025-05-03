146724abb867c685ea9e9e46a3abfe232dbb1a26
davidwanlongwu <davidwu.web@gmail.com>
2025-05-03 12:01:52 -0600

    adding new organization id column to invoices

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -24,6 +24,7 @@ export const Invoices = pgTable("invoices", {
   value: integer("value").notNull(),
   description: text("description").notNull(),
   userId: text("userId").notNull(),
+  organizationId: text("organizationId"),
   customerId: integer("customerId")
     .notNull()
     .references(() => Customers.id),
@@ -36,4 +37,5 @@ export const Customers = pgTable("customers", {
   name: text("name").notNull(),
   email: text("email").notNull(),
   userId: text("userId").notNull(),
+  organizationId: text("organizationId"),
 });
