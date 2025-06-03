0129afe0022155ee008e21168c854d81ef0607cc
davidwanlongwu <davidwu.web@gmail.com>
2025-06-03 16:08:06 -0600

    adjust text and button style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col h-screen justify-center max-w-5xl mx-auto text-center gap-8">
-      <h1 className="text-5xl font-bold">Invoice Notebook</h1>
+      <h1 className="text-5xl font-bold text-[#f8b500]">Invoice Notebook</h1>
       <p>
-        <Button asChild>
-          <Link href="/dashboard">Sign In</Link>
+        <Button asChild variant="ghost">
+          <Link
+            href="/dashboard"
+            className="bg-[#478384] text-white px-4 py-2 rounded-md hover:bg-[#376c6c]"
+          >
+            Sign In
+          </Link>
         </Button>
       </p>
     </main>
